22d08532e1cb21cccfb4e7f346e45d46df549fbb
alexeykush <alexey.kushnarov@ukr.net>
2020-07-08 16:15:47 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const MAX_ITEMS = 80;
+
+const createItems = () => new Array(20).fill(0);
+
+const App = () => {
+    const [items, setItems] = useState(createItems());
+
+    const loadMore = useCallback(page => {
+        console.log(page);
+        setTimeout(() => setItems(items => [...items, ...createItems()]), 1500);
+    }, []);
+
+    const mappedItems = items.map((_, i) => <div className="item" key={i}>#{i + 1}</div>);
+
+    return (
+        <div className="container">
+            <InfiniteScroll
+                pageStart={0}
+                loadMore={loadMore}
+                hasMore={items.length < MAX_ITEMS}
+                loader={<div className="loader" key={0}>Loading ...</div>}
+            >
+                {mappedItems}
+            </InfiniteScroll>
+        </div>
+    );
+};
 
 export default App;
